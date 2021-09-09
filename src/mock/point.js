d4c5610b97c1e775af64931919a01d4b165c652f
hoblex <hoblex@gmail.com>
2021-09-09 17:13:15 +0300

    проверяется корректность выполнения функций генерации случайного числа

--- src/mock/point.js
+++ src/mock/point.js
@@ -6,15 +6,22 @@ dayjs.extend(utc);
 // Функция из интернета по генерации случайного числа из диапазона
 // Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
 const getRandomInteger = (a = 0, b = 1) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-
-  return Math.floor(lower + Math.random() * (upper - lower + 1));
+  if(!(Number.isInteger(a) && Number.isInteger(b))) {
+    throw new Error ('Integer numbers expected');
+  } else {
+    const lower = Math.ceil(Math.min(a, b));
+    const upper = Math.floor(Math.max(a, b));
+    return Math.floor(lower + Math.random() * (upper - lower + 1));
+  }
 };
 
 const generateRandomItem = (list = []) => {
-  const randomIndex = getRandomInteger(0, list.length - 1);
-  return list[randomIndex];
+  if (list.length === 0) {
+    throw new Error ('Integer numbers expected');
+  } else {
+    const randomIndex = getRandomInteger(0, list.length - 1);
+    return list[randomIndex];
+  }
 };
 
 const ADDITIONAL_OPTIONALS = [
