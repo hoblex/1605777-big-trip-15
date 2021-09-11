4822df9dd54ce578948d00ec4517eadaf5dcf2e0
Aleksandr Alekseev <hoblex@gmail.com>
2021-09-12 00:33:24 +0300

    добавляет применение наследования к простым классам

--- src/view/list-empty.js
+++ src/view/list-empty.js
@@ -1,27 +1,11 @@
-import {createElement} from './utils';
+import AbstractView from './abstract';
 
 const listEmptyContainer = () => (
   '<p class="trip-events__msg">Click New Event to create your first point</p>'
 );
 
-export default class ListEmpty {
-  constructor() {
-    this._element = null;
-  }
-
+export default class ListEmpty extends AbstractView {
   getTemplate() {
     return listEmptyContainer();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
