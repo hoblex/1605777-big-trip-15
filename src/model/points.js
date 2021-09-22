2e19ec3ee1d4c9b35da544bf5ec72d85a1b7a394
Aleksandr Alekseev <hoblex@gmail.com>
2021-09-22 22:24:51 +0300

    добавляет настройки dayjs

--- src/model/points.js
+++ src/model/points.js
@@ -1,5 +1,7 @@
 import AbstractObserver from '../utils/abstract-observer.js';
 import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+dayjs.extend(duration);
 
 export default class Points extends AbstractObserver {
   constructor() {
