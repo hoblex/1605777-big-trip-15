22adfba06a8916ab533d4918b4c1bb16dd1133e8
hoblex <hoblex@gmail.com>
2021-09-22 18:41:23 +0300

    исравляет замечания eslint

--- src/utils/statistics.js
+++ src/utils/statistics.js
@@ -1,5 +1,5 @@
 export const countMoneyByType = (points, type) =>
-  points.filter((item) => item.pointType === type.toLowerCase()).reduce(((summa, point) => summa + point.pointCost), 0)
+  points.filter((item) => item.pointType === type.toLowerCase()).reduce(((summa, point) => summa + point.pointCost), 0);
 
 export const countAmountByType =
   (points, type) => points.filter((point) => point.pointType === type.toLowerCase()).length;
