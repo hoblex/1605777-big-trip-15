75092af4c85b1c9015864e9d1a01d01592003c6b
hoblex <hoblex@gmail.com>
2021-09-23 13:23:30 +0300

    исправляет работу компонента Информация о маршруте

--- src/view/route-info.js
+++ src/view/route-info.js
@@ -5,18 +5,29 @@ const BEGINNING_FORMAT_STRING ='MMM DD';
 const END_FORMAT_SAME_MONTH = 'DD';
 const END_FORMAT_OTHER_MONTH ='MMM DD';
 
-export const createTripInfo = (cityList, dateList, price) => {
-  const arr = Array.from(dateList);
-  const [tripBegin] = arr[0];
-  const [, tripEnd] = arr[arr.length - 1];
+const createDateInfo = (dateList) => {
+  const dateArray = Array.from(dateList);
+  const [tripBegin] = dateArray[0];
+  const [, tripEnd] = dateArray[dateArray.length - 1];
   const tripDateBeginEnd = tripBegin.month() === tripEnd.month()
     ? `${dayjs(tripBegin).format(BEGINNING_FORMAT_STRING)}&nbsp;&mdash;&nbsp;${dayjs(tripEnd).format(END_FORMAT_SAME_MONTH)}`
     : `${dayjs(tripBegin).format(BEGINNING_FORMAT_STRING)}&nbsp;&mdash;&nbsp;${dayjs(tripEnd).format(END_FORMAT_OTHER_MONTH)}`;
 
+  return tripDateBeginEnd;
+};
+
+const createCityList = (cityList) => {
+  const cityArray = Array.from(cityList);
+  return [cityArray[0], '...', cityArray[cityArray.length - 1]].join(' &mdash; ');
+
+};
+
+export const createTripInfo = (cityList, dateList, price) => {
+
   return `<section class="trip-main__trip-info  trip-info">
     <div class="trip-info__main">
-    <h1 class="trip-info__title">${Array.from(cityList).join(' &mdash; ')}</h1>
-    <p class="trip-info__dates">${tripDateBeginEnd}</p>
+    <h1 class="trip-info__title">${createCityList(cityList)}</h1>
+    <p class="trip-info__dates">${createDateInfo(dateList)}</p>
     </div>
     <p class="trip-info__cost">
     Total: &euro;&nbsp;<span class="trip-info__cost-value">${price}</span>
